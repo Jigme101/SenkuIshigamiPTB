996a19ab5be7b21df2cd7dd8a43bf37d80016a15
Jigme101 <91387153+Jigme101@users.noreply.github.com>
2022-05-16 08:07:56 +0530

    Update config.py

--- Senku/config.py
+++ Senku/config.py
@@ -4,7 +4,7 @@ import os
 
 
 def get_user_list(config, key):
-    with open("{}/Senku/{}".format(os.getcwd(), config), "r") as json_file:
+    with open("{}/Muzan/{}".format(os.getcwd(), config), "r") as json_file:
         return json.load(json_file)[key]
 
 
@@ -14,17 +14,17 @@ class Config(object):
     # REQUIRED
     # Login to https://my.telegram.org and fill in these slots with the details given by it
 
-    API_ID = 123456  # integer value, dont use ""
-    API_HASH = "awoo"
-    TOKEN = "BOT_TOKEN"  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
-    OWNER_ID = 1606221784  # If you dont know, run the bot and do /id in your private chat with it, also an integer
-    OWNER_USERNAME = "excrybaby"
-    SUPPORT_CHAT = "emikosupport"  # Your own group for support, do not add the @
+    API_ID = 9856444  # integer value, dont use ""
+    API_HASH = 94be85dd7928db3e05cceb14356cf6e0
+    TOKEN = 5340692280:AAEsSzD9Gap5r4KouQV7HpCx6D0Pv3HpuQQ  # This var used to be API_KEY but it is now TOKEN, adjust accordingly.
+    OWNER_ID = 1173417762  # If you dont know, run the bot and do /id in your private chat with it, also an integer
+    OWNER_USERNAME = "MrxDabi"
+    SUPPORT_CHAT = "DemonDomain"  # Your own group for support, do not add the @
     JOIN_LOGGER = (
-        -1001432609692
+        -1001574494907
     )  # Prints any new group the bot is added to, prints just the name and ID.
     EVENT_LOGS = (
-        -1001150905176
+        -1001547974075
     )  # Prints information like gbans, sudo promotes, AI enabled disable states that may help in debugging and shit
 
     # RECOMMENDED
@@ -34,7 +34,7 @@ class Config(object):
     WEBHOOK = False
     INFOPIC = True
     URL = None
-    SPAMWATCH_API = ""  # go to support.spamwat.ch to get key
+    SPAMWATCH_API = "5JTnarcavaWRcI1KteUMsYQCeSsNeT0ZJswK1~G73Imr3~f6BFTVTmmT74f20C9q"  # go to support.spamwat.ch to get key
     SPAMWATCH_SUPPORT_CHAT = "@SpamWatchSupport"
 
     # OPTIONAL
@@ -55,16 +55,16 @@ class Config(object):
     WORKERS = (
         8  # Number of subthreads to use. Set as number of threads your processor uses
     )
-    BAN_STICKER = ""  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
+    BAN_STICKER = "CAACAgUAAxkBAAEMJqZigbjwBwKPGfmtt-DUyAeH4WMFHgACqwYAAkM1EVTmSu69kgJ6JiQE"  # banhammer marie sticker id, the bot will send this sticker before banning or kicking a user in chat.
     ALLOW_EXCL = True  # Allow ! commands as well as / (Leave this to true so that blacklist can work)
     CASH_API_KEY = (
         "awoo"  # Get your API key from https://www.alphavantage.co/support/#api-key
     )
-    TIME_API_KEY = "awoo"  # Get your API key from https://timezonedb.com/api
+    TIME_API_KEY = "GKVN0LIKEHZA"  # Get your API key from https://timezonedb.com/api
     WALL_API = (
         "awoo"  # For wallpapers, get one from https://wall.alphacoders.com/api.php
     )
-    AI_API_KEY = "awoo"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
+    AI_API_KEY = "1173417762-KUKIVF2lNo8mV6"  # For chatbot, get one from https://coffeehouse.intellivoid.net/dashboard
     BL_CHATS = []  # List of groups that you want blacklisted.
     SPAMMERS = None
 
